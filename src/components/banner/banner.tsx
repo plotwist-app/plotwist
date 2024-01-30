2959617fe3a8395a14b17fa81d3ad6ffd1a81de7
Luiz Henrique <7henrique18@gmail.com>
2024-01-30 18:59:49 -0300

    refactor: banner styles for improved visual clarity

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -4,14 +4,14 @@ type BannerProps = {
 
 export const Banner = ({ url }: BannerProps) => {
   return (
-    <div className={`h-[80vh] overflow-hidden`}>
+    <div className={`h-[80vh] overflow-hidden border-b`}>
       <div
         style={{
           backgroundImage: `url('${url}')`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
-        className="h-full w-full border-b brightness-50"
+        className="h-full w-full brightness-50"
         data-testid="banner"
       />
     </div>
