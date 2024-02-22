414bd6d795b3df56a632c489b47d028729ebf6e8
LuizGarbini <garbasneto@gmail.com>
2024-02-22 20:50:49 -0300

    fix(i18n): remove unnecessary description_required field from list form schema definition

--- src/app/[lang]/app/lists/_components/list-form-schema.tsx
+++ src/app/[lang]/app/lists/_components/list-form-schema.tsx
@@ -4,9 +4,7 @@ import { z } from 'zod'
 export const listFormSchema = (dictionary: Dictionary) =>
   z.object({
     name: z.string().min(1, dictionary.create_new_list_form.name_required),
-    description: z
-      .string()
-      .min(1, dictionary.create_new_list_form.description_required),
+    description: z.string(),
   })
 
 export type ListFormValues = z.infer<ReturnType<typeof listFormSchema>>
