5f6be7cfb4dd7dd11db65f3458f36c111ad3cd85
Luiz Henrique <7henrique18@gmail.com>
2024-02-23 17:52:27 -0300

    fix(layout): remove unnecessary closeButton from Toaster component

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
             dictionary={dictionary}
           >
             {children}
-            <Toaster closeButton />
+            <Toaster />
           </LanguageContextProvider>
         </AppContextProvider>
       </body>
