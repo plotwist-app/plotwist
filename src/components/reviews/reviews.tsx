f5637366ee094b36b5de0937529bc4e65355cd29
prenansb <prenansb@gmail.com>
2024-03-04 23:35:29 -0300

    refactor(reviews): move get reviews query to service

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -3,14 +3,13 @@
 import { MediaType } from '@/types/supabase/media-type'
 import { TvSeriesDetails } from '@/services/tmdb/requests/tv-series/details'
 import { MovieDetails } from '@/services/tmdb/requests/movies/details'
-import { supabase } from '@/services/supabase'
 import { useQuery } from '@tanstack/react-query'
-import { Review } from '@/types/supabase/reviews'
 import {
   ReviewItem,
   ReviewItemSkeleton,
 } from '@/components/reviews/review-item'
 import { ReviewForm } from '@/components/reviews/review-form'
+import { getReviewsService } from '@/services/api/reviews/get-reviews'
 
 type TmdbItem = TvSeriesDetails | MovieDetails
 
@@ -20,16 +19,9 @@ export type ReviewsProps = {
 }
 
 export const Reviews = ({ tmdbItem, mediaType }: ReviewsProps) => {
-  const { data: response, isLoading } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: [tmdbItem.id, mediaType],
-    queryFn: async () =>
-      supabase
-        .from('reviews_with_replies')
-        .select('*')
-        .eq('tmdb_id', tmdbItem.id)
-        .order('id', { ascending: false })
-        .eq('media_type', mediaType)
-        .returns<Review[]>(),
+    queryFn: async () => getReviewsService({ id: tmdbItem.id, mediaType }),
   })
 
   if (isLoading) {
@@ -42,11 +34,11 @@ export const Reviews = ({ tmdbItem, mediaType }: ReviewsProps) => {
     )
   }
 
-  if (!response?.data) return <></>
+  if (!data) return <></>
 
   return (
     <section className="space-y-8">
-      {response.data.map((review) => (
+      {data.map((review) => (
         <ReviewItem
           key={review.id}
           review={review}
