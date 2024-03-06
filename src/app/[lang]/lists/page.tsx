d64c5e4dad79b9fe7f3fbf5f007bccee04ad5bdb
Luiz Henrique <7henrique18@gmail.com>
2024-03-06 16:50:02 -0300

    feat(lists): add function to generate page metadata

--- src/app/[lang]/lists/page.tsx
+++ src/app/[lang]/lists/page.tsx
@@ -1,18 +1,40 @@
 import { PageProps } from '@/types/languages'
 import { Lists } from './_components/lists'
 import { getDictionary } from '@/utils/dictionaries'
+import { Metadata } from 'next'
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const {
+    lists_page: { title, description },
+  } = await getDictionary(params.lang)
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+    twitter: {
+      title,
+      description,
+    },
+  }
+}
 
 const ListsPage = async ({ params }: PageProps) => {
-  const dictionary = await getDictionary(params.lang)
+  const {
+    lists_page: { title, description },
+  } = await getDictionary(params.lang)
 
   return (
     <div className="mx-auto max-w-6xl space-y-4">
       <div className="flex items-center justify-between">
         <div>
-          <h1 className="text-2xl font-bold">{dictionary.lists_page.title}</h1>
-          <p className="text-muted-foreground">
-            {dictionary.lists_page.description}
-          </p>
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <p className="text-muted-foreground">{description}</p>
         </div>
       </div>
 
