25603de9581571569c28081133b75925cdc4b670
Luiz Henrique <7henrique18@gmail.com>
2024-03-05 21:31:16 -0300

    feat(header): add responsive behavior for navigation options

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,26 +5,41 @@ import { CommandSearch } from '../command-search'
 import { SettingsDropdown } from '../settings-dropdown'
 import { HeaderNavigationMenu } from './header-navigation-menu'
 import { useLanguage } from '@/context/language'
+import { useMediaQuery } from '@uidotdev/usehooks'
+import { HeaderNavigationDrawer } from './haeder-navigation-drawer'
 
 export const Header = () => {
   const { language } = useLanguage()
+  const isDesktop = useMediaQuery('(min-width: 1080px)')
+
+  if (isDesktop) {
+    return (
+      <header className="flex justify-between">
+        <div className="flex items-center gap-4">
+          <Link href={`/${language}/`}>
+            <h1 className="text-xl font-semibold">[TMDB]</h1>
+          </Link>
+
+          <HeaderNavigationMenu />
+        </div>
+
+        <div className="flex gap-2">
+          <CommandSearch />
+          <div>
+            <SettingsDropdown />
+          </div>
+        </div>
+      </header>
+    )
+  }
 
   return (
-    <header className="flex justify-between">
-      <div className="flex items-center gap-4">
-        <Link href={`/${language}/`}>
-          <h1 className="text-xl font-semibold">[TMDB]</h1>
-        </Link>
+    <header className="flex w-full justify-between">
+      <Link href={`/${language}/`}>
+        <h1 className="text-xl font-semibold">[TMDB]</h1>
+      </Link>
 
-        <HeaderNavigationMenu />
-      </div>
-
-      <div className="flex gap-2">
-        <CommandSearch />
-        <div>
-          <SettingsDropdown />
-        </div>
-      </div>
+      <HeaderNavigationDrawer />
     </header>
   )
 }
