80a1bf8f318d01ade3680cdefd52475c6d22faa2
Luiz Henrique <7henrique18@gmail.com>
2024-03-15 22:00:28 -0300

    feat(app): update robots function to remove APP_URL from sitemap URLs

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,9 @@ import { APP_URL } from '../../constants'
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
   const sitemapItems = await sitemap()
-  const allow = sitemapItems.map((sitemapItem) => sitemapItem.url)
-
-  console.log({ allow })
+  const allow = sitemapItems.map((sitemapItem) =>
+    sitemapItem.url.replace(APP_URL, ''),
+  )
 
   return {
     rules: {
