8e4c59218f338acfd8ca27878be6b28207770d76
Luiz Henrique <7henrique18@gmail.com>
2024-03-15 21:49:58 -0300

    chore(constants.ts): update production app URL for plotwist app

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const APP_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://plottwist.vercel.app'
+    ? 'https://plotwist.app'
     : 'http://localhost:3000'
 
 export const TESTING_USER = {
