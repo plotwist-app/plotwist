d12a35116638680c2cc8d167cf14e1ccf0f38d34
Luiz Henrique <7henrique18@gmail.com>
2024-03-19 20:30:39 -0300

    refactor: remove unnecessary Supabase middleware client creation and session retrieval

--- src/services/api/users/get-user.ts
+++ src/services/api/users/get-user.ts
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 
 export const getUserService = async () => {
   const supabase = createServerComponentClient({ cookies })
+
   const user = await supabase.auth.getUser()
 
   return user
