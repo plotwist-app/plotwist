b7aa5fb568d5518b4e7881b3f3de51d321c81f2a
Luiz Henrique <7henrique18@gmail.com>
2024-04-05 10:08:18 -0300

    fix: enable home price

--- apps/web/src/app/[lang]/_components/home-prices/home-prices.tsx
+++ apps/web/src/app/[lang]/_components/home-prices/home-prices.tsx
@@ -127,7 +127,7 @@ export const HomePrices = () => {
                   </HomePrice.Benefits>
                 </HomePrice.Content>
 
-                <Button type="submit" disabled>
+                <Button type="submit">
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger asChild>
