91571d7a6ffdc565fd6fbbe4deced1983dbd193a
Luiz Henrique <7henrique18@gmail.com>
2024-04-17 20:31:01 -0300

    refactor(package.json, layout): remove next-view-transitions dependency and component, simplifying layout structure

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next'
-import { ViewTransitions } from 'next-view-transitions'
 import { Space_Grotesk as SpaceGrotesk } from 'next/font/google'
 import { Language } from '@/types/languages'
 import { GTag } from '@/components/gtag'
@@ -26,20 +25,18 @@ export default async function RootLayout({
   params: { lang: Language }
 }) {
   return (
-    <ViewTransitions>
-      <html
-        lang={params.lang}
-        className={spaceGrotesk.className}
-        suppressHydrationWarning
-      >
-        <head>
-          <link rel="icon" href="/logo-white.svg" />
-          <meta name="theme-color" content="#09090b" />
-          <GTag />
-        </head>
+    <html
+      lang={params.lang}
+      className={spaceGrotesk.className}
+      suppressHydrationWarning
+    >
+      <head>
+        <link rel="icon" href="/logo-white.svg" />
+        <meta name="theme-color" content="#09090b" />
+        <GTag />
+      </head>
 
-        <body className="bg-background antialiased">{children}</body>
-      </html>
-    </ViewTransitions>
+      <body className="bg-background antialiased">{children}</body>
+    </html>
   )
 }
