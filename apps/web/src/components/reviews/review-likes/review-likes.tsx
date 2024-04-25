4cebeae49eab6073e3ab373db6948a26489ff93a
Ranieri Marques <raniere852@outlook.com>
2024-04-25 00:13:38 -0300

    feat: add modal of users who liked the review

--- apps/web/src/components/reviews/review-likes/review-likes.tsx
+++ apps/web/src/components/reviews/review-likes/review-likes.tsx
@@ -1,11 +1,37 @@
+import { ProBadge } from '@/components/pro-badge'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useLanguage } from '@/context/language'
 import { getLikesService } from '@/services/api/likes/get-likes'
+import { getProfilesById } from '@/services/api/profiles/get-profiles-by-id'
+import { tmdbImage } from '@/utils/tmdb/image'
+import { DialogTitle } from '@radix-ui/react-dialog'
 import { useQuery } from '@tanstack/react-query'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function ReviewLikes({ reviewId }: { reviewId: string }) {
+  const { language } = useLanguage()
   const { data: likes } = useQuery({
     queryKey: ['likes', reviewId],
     queryFn: async () =>
       getLikesService({ id: reviewId, entityType: 'REVIEW' }),
+  })
+
+  const likedUsersId = likes?.data.map((like) => like.user_id) ?? []
+
+  const { data: profiles } = useQuery({
+    queryKey: ['profiles', reviewId],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      return getProfilesById(likedUsersId)
+    },
+    enabled: Boolean(likedUsersId?.length),
   })
 
   if (!likes?.data) {
@@ -17,12 +43,64 @@ export function ReviewLikes({ reviewId }: { reviewId: string }) {
   if (likes.count === 0) return null
 
   return (
-    <div
-      className={
-        'absolute -bottom-3.5 right-2 rounded-full border bg-muted px-3 py-1 text-xs'
-      }
-    >
-      ❤ <span className="ml-1">{likes.count}</span>
-    </div>
+    <Dialog>
+      <DialogTrigger>
+        <div className="absolute -bottom-3.5 right-2 rounded-full border bg-muted px-3 py-1 text-xs hover:bg-muted/60">
+          ❤ <span className="ml-1">{likes.count}</span>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="flex max-h-[642px] flex-col overflow-y-auto">
+        <DialogHeader className="mb-2">
+          <DialogTitle>Curtidas</DialogTitle>
+        </DialogHeader>
+        {!profiles &&
+          Array.from({ length: 5 }).map((_, index) => (
+            <div key={index} className="flex items-center gap-3">
+              <Skeleton className="size-10 rounded-full" />
+              <Skeleton className="h-5 w-40" />
+              <Skeleton className="ml-auto h-4 w-20" />
+            </div>
+          ))}
+
+        {profiles?.map((profile) => (
+          <div key={profile.id} className="flex items-center">
+            <Link
+              href={`/${language}/${profile.username}`}
+              className="flex items-center overflow-hidden"
+            >
+              <div className="relative flex aspect-square size-10 shrink-0 items-center justify-center overflow-hidden rounded-full border bg-background/50 shadow">
+                {profile.banner_path ? (
+                  <Image
+                    fill
+                    className="object-cover"
+                    src={tmdbImage(profile.banner_path)}
+                    alt={profile.username}
+                    sizes="100%"
+                  />
+                ) : (
+                  profile.username[0]
+                )}
+              </div>
+              <span className="ml-3 mr-2 truncate text-sm">
+                {profile.username}
+              </span>
+            </Link>
+
+            {profile.subscription_type === 'PRO' && (
+              <Link href={`/${language}/pricing`}>
+                <ProBadge />
+              </Link>
+            )}
+
+            <Link
+              href={`/${language}/${profile.username}`}
+              className="ml-auto whitespace-nowrap pl-8 text-xs text-muted-foreground hover:underline"
+            >
+              Ver perfil
+            </Link>
+          </div>
+        ))}
+      </DialogContent>
+    </Dialog>
   )
 }
