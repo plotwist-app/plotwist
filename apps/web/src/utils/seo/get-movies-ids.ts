cda0d539085465ab4cee6453dd235441ca281fe4
Luiz Henrique <7henrique18@gmail.com>
2024-05-09 22:27:11 -0300

    feat(seo): update PAGES_BY_TYPE to improve SEO performance

--- apps/web/src/utils/seo/get-movies-ids.ts
+++ apps/web/src/utils/seo/get-movies-ids.ts
@@ -2,11 +2,12 @@ import { tmdb } from '@plotwist/tmdb'
 import { homeMovies } from '@/app/[lang]/page'
 import { Language } from '@/types/languages'
 
-const PAGES_BY_TYPE = 5
+const PAGES_BY_TYPE = 30
 
 export const getMoviesIds = async (language: Language) => {
   const types = ['now_playing', 'popular', 'top_rated', 'upcoming'] as const
 
+  console.time()
   const lists = await Promise.all(
     Array.from({ length: PAGES_BY_TYPE }).map(
       async (_, index) =>
@@ -22,12 +23,17 @@ export const getMoviesIds = async (language: Language) => {
         ),
     ),
   )
+  console.timeEnd()
 
+  console.time()
   const results = lists.flatMap((list) => list.map((list) => list.results))
   const ids = results.flatMap((result) => result.map((movie) => movie.id))
 
   const combinedIds = [...Object.values(homeMovies), ...ids]
   const uniqueIds = Array.from(new Set(combinedIds))
+  console.timeEnd()
+
+  console.log({ length: uniqueIds.length })
 
   return uniqueIds
 }
