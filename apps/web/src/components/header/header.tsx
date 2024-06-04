82f02f4b6a35ed90491b3cf10c46aa0c72db5428
Heitor <145717927+Heitors09@users.noreply.github.com>
2024-06-04 09:31:15 -0300

    feat: add button signup in header (#195)

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -5,8 +5,15 @@ import { SettingsDropdown } from '../settings-dropdown'
 import { HeaderNavigationMenu } from './header-navigation-menu'
 import { HeaderNavigationDrawer } from './header-navigation-drawer'
 import { Logo } from '../logo'
+import { useAuth } from '@/context/auth'
+import Link from 'next/link'
+import { useLanguage } from '@/context/language'
+import { User } from 'lucide-react'
 
 export const Header = () => {
+  const { user } = useAuth()
+  const { language, dictionary } = useLanguage()
+
   return (
     <>
       <header className="hidden justify-between lg:flex">
@@ -16,6 +23,19 @@ export const Header = () => {
         </div>
 
         <div className="flex gap-2">
+          {!user && (
+            <Link
+              href={`/${language}/signup`}
+              className="block cursor-pointer select-none space-y-1 rounded-md p-3 text-sm leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+            >
+              <div className="flex items-center gap-2">
+                <User width={12} height={12} />
+                <p className="text-sm font-medium leading-none">
+                  {dictionary.create_account}
+                </p>
+              </div>
+            </Link>
+          )}
           <CommandSearch />
           <SettingsDropdown />
         </div>
