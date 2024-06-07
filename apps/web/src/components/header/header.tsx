e675bbffeff1895ee827d215932b179acf599b3f
Heitors09 <heitorao32@gmail.com>
2024-06-07 15:03:11 -0300

    feat: add profile in header

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -9,10 +9,13 @@ import { useAuth } from '@/context/auth'
 import Link from 'next/link'
 import { useLanguage } from '@/context/language'
 import { User } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { tmdbImage } from '@/utils/tmdb/image'
 
 export const Header = () => {
   const { user } = useAuth()
   const { language, dictionary } = useLanguage()
+  const initial = user?.username[0]?.toUpperCase()
 
   return (
     <>
@@ -22,8 +25,8 @@ export const Header = () => {
           <HeaderNavigationMenu />
         </div>
 
-        <div className="flex gap-2">
-          {!user && (
+        <div className="flex items-center gap-2">
+          {!user ? (
             <Link
               href={`/${language}/signup`}
               className="block cursor-pointer select-none space-y-1 rounded-md p-3 text-sm leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
@@ -32,6 +35,26 @@ export const Header = () => {
                 <User width={12} height={12} />
                 <p className="text-sm font-medium leading-none">
                   {dictionary.create_account}
+                </p>
+              </div>
+            </Link>
+          ) : (
+            <Link
+              href={`/${language}/${user.username}`}
+              className="block cursor-pointer select-none space-y-1 rounded-md p-2 text-sm leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+            >
+              <div className="flex items-center">
+                <Avatar className="mr-2 h-6 w-6 border text-[10px]">
+                  {user.image_path && (
+                    <AvatarImage
+                      src={tmdbImage(user.image_path, 'w500')}
+                      className="object-cover"
+                    />
+                  )}
+                  <AvatarFallback>{initial}</AvatarFallback>
+                </Avatar>
+                <p className="text-sm font-medium leading-none">
+                  {user.username}
                 </p>
               </div>
             </Link>
