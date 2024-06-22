b096e91dc22f39f5413d09697067701a0fadbd5f
Heitor <145717927+Heitors09@users.noreply.github.com>
2024-06-22 11:35:59 -0300

    improvement: optimized ReviewLikes request (#204)

--- apps/web/src/services/api/likes/get-likes.ts
+++ apps/web/src/services/api/likes/get-likes.ts
@@ -10,11 +10,12 @@ export const getLikesService = async ({ id, entityType }: GetLikeParams) => {
 
   const response = await supabase
     .from('likes')
-    .select('*', { count: 'exact' })
+    .select('*, profiles (*)', {
+      count: 'exact',
+    })
     .eq('entity_type', entityType)
     .eq(idType, id)
 
   if (response.error) throw new Error(response.error.message)
-
   return response
 }
