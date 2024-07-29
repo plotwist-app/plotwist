78848d18af4e6729e615755bd2d8d50b336629f2
Luiz Henrique <7henrique18@gmail.com>
2024-07-29 10:01:17 -0300

    style(ui): update poster card layout to remove year - improved appearance and consistency

--- apps/web/src/app/[lang]/movies/[id]/_components/movie-collection-dialog.tsx
+++ apps/web/src/app/[lang]/movies/[id]/_components/movie-collection-dialog.tsx
@@ -55,7 +55,7 @@ export const MovieCollectionDialog = ({
             </DialogTitle>
           </DialogHeader>
 
-          <div className="mt-2 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3">
+          <div className="mt-2 grid grid-cols-2 grid-cols-3 gap-4">
             {parts.map((movie) => (
               <Link href={`/${language}/movies/${movie.id}`} key={movie.id}>
                 <PosterCard.Root>
@@ -63,13 +63,6 @@ export const MovieCollectionDialog = ({
                     src={tmdbImage(movie.poster_path, 'w500')}
                     alt={movie.title}
                   />
-
-                  <PosterCard.Details>
-                    <PosterCard.Title>{movie.title}</PosterCard.Title>
-                    <PosterCard.Year>
-                      {movie.release_date.split('-')[0]}
-                    </PosterCard.Year>
-                  </PosterCard.Details>
                 </PosterCard.Root>
               </Link>
             ))}
