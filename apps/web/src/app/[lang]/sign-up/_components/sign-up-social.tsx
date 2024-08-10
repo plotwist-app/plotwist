01155a3c2e7cc8961981a7abf65dd9422632c967
Luiz Henrique <7henrique18@gmail.com>
2024-08-09 22:05:51 -0300

    style: update social sign-up buttons text to use dictionary values

--- apps/web/src/app/[lang]/sign-up/_components/sign-up-social.tsx
+++ apps/web/src/app/[lang]/sign-up/_components/sign-up-social.tsx
@@ -1,40 +1,40 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { useLanguage } from '@/context/language'
 import Link from 'next/link'
 import { tv } from 'tailwind-variants'
+import { Button } from '@/components/ui/button'
+import { useLanguage } from '@/context/language'
 
 export const socialButton = tv({
   base: 'rounded-lg items-center border text-sm bg-background px-4 py-2 flex gap-2 justify-center cursor-pointer shadow-sm w-full font-semib',
 })
 
 export const SignUpSocial = () => {
-  const { language } = useLanguage()
+  const { language, dictionary } = useLanguage()
 
   return (
     <div className="flex flex-col gap-2">
       <Button className="border bg-background text-foreground shadow-sm hover:bg-muted/50">
         <img src="/assets/google.svg" className="mr-2 size-4" alt="google" />
-        Continue com Google
+        {dictionary.continue_with_google}
       </Button>
 
       <Button className="border bg-background text-foreground shadow-sm hover:bg-muted/50 dark:[&_img]:invert">
-        <img src="/assets/x.svg" className="mr-2 size-4" alt="x" /> Continue com
-        X
+        <img src="/assets/x.svg" className="mr-2 size-4" alt="x" />{' '}
+        {dictionary.continue_with_x}
       </Button>
 
       <div className="flex items-center">
         <div className="flex-1 border-t" />
-        <span className="m-4 text-sm font-medium text-muted-foreground/50">
-          OU
+        <span className="m-4 text-sm font-medium uppercase text-muted-foreground/50">
+          {dictionary.or}
         </span>
         <div className="flex-1 border-t" />
       </div>
 
       <Button asChild>
         <Link href={`/${language}/sign-up?provider=email`}>
-          Continue com e-mail
+          {dictionary.continue_with_email}
         </Link>
       </Button>
     </div>
