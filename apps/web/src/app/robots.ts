aa325feb94dee0472333bc0d28eb2b84cf331b11
Luiz Henrique <7henrique18@gmail.com>
2024-09-09 16:41:48 -0300

    chore(vscode): Update vscode settings and biome.json configurations.

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -1,13 +1,13 @@
-import { MetadataRoute } from 'next'
-import { APP_URL } from '../../constants'
+import { MetadataRoute } from "next";
+import { APP_URL } from "../../constants";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: `${APP_URL}/sitemap.xml`,
-    host: APP_URL,
-  }
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+		},
+		sitemap: `${APP_URL}/sitemap.xml`,
+		host: APP_URL,
+	};
 }
--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,28 +1,28 @@
-import { MetadataRoute } from 'next'
-import { SUPPORTED_LANGUAGES } from '../../languages'
-import { APP_URL } from '../../constants'
+import { MetadataRoute } from "next";
+import { SUPPORTED_LANGUAGES } from "../../languages";
+import { APP_URL } from "../../constants";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const sitemaps = SUPPORTED_LANGUAGES.map((language) => [
-    {
-      url: `${APP_URL}/${language.value}/sitemap.xml`,
-      changeFrequency: 'weekly' as const,
-      lastModified: new Date().toISOString(),
-      priority: 1,
-    },
-    {
-      url: `${APP_URL}/${language.value}/movies/sitemap.xml`,
-      changeFrequency: 'weekly' as const,
-      lastModified: new Date().toISOString(),
-      priority: 1,
-    },
-    {
-      url: `${APP_URL}/${language.value}/tv-series/sitemap.xml`,
-      changeFrequency: 'weekly' as const,
-      lastModified: new Date().toISOString(),
-      priority: 1,
-    },
-  ]).flatMap((sitemap) => sitemap)
+	const sitemaps = SUPPORTED_LANGUAGES.map((language) => [
+		{
+			url: `${APP_URL}/${language.value}/sitemap.xml`,
+			changeFrequency: "weekly" as const,
+			lastModified: new Date().toISOString(),
+			priority: 1,
+		},
+		{
+			url: `${APP_URL}/${language.value}/movies/sitemap.xml`,
+			changeFrequency: "weekly" as const,
+			lastModified: new Date().toISOString(),
+			priority: 1,
+		},
+		{
+			url: `${APP_URL}/${language.value}/tv-series/sitemap.xml`,
+			changeFrequency: "weekly" as const,
+			lastModified: new Date().toISOString(),
+			priority: 1,
+		},
+	]).flatMap((sitemap) => sitemap);
 
-  return [...sitemaps]
+	return [...sitemaps];
 }
