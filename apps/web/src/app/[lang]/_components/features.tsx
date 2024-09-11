67172b16045c3abca0e864c747638f5186d9d7fc
Luiz Henrique <7henrique18@gmail.com>
2024-09-10 21:54:13 -0300

    feat(features): add fade effect to feature section for UX enhancement

--- apps/web/src/app/[lang]/_components/features.tsx
+++ apps/web/src/app/[lang]/_components/features.tsx
@@ -1,4 +1,7 @@
+import { ReviewItem } from '@/components/reviews/review-item'
+import { getPopularReviewsService } from '@/services/api/reviews'
 import { Language } from '@/types/languages'
+import { BlurFade } from '@plotwist/ui/components/magicui/blur-fade'
 import { List, Pencil, Users } from 'lucide-react'
 import { ComponentProps } from 'react'
 
@@ -18,49 +21,74 @@ const Feature = {
   ),
 }
 
-export const Features = async () => {
+const BLUR_FADE_DELAY = 0.1
+
+type FeaturesProps = { language: Language }
+
+export const Features = async ({ language }: FeaturesProps) => {
+  const popularReviews = await getPopularReviewsService({
+    language,
+    period: 'all_time',
+  })
+
   return (
     <section className="mx-auto max-w-6xl pb-32 space-y-6">
       <div className="flex justify-between gap-4">
-        <h2 className="font-bold text-4xl w-1/2">Made for modern users</h2>
+        <BlurFade delay={BLUR_FADE_DELAY} className="w-1/4" inView>
+          <h2 className="font-bold text-4xl ">Made for modern users</h2>
+        </BlurFade>
 
-        <p className="text-muted-foreground w-3/4">
-          Linear is shaped by the practices and principles that distinguish
-          world-class product teams from the rest: relentless focus, fast
-          execution, and a commitment to the quality of craft.
-        </p>
+        <BlurFade delay={BLUR_FADE_DELAY * 2} className="w-3/4" inView>
+          <p className="text-muted-foreground">
+            Linear is shaped by the practices and principles that distinguish
+            world-class product teams from the rest: relentless focus, fast
+            execution, and a commitment to the quality of craft.
+          </p>
+        </BlurFade>
       </div>
 
       <ul className="grid grid-cols-3 gap-4">
-        <Feature.Root>
-          <div className="aspect-square bg-muted"></div>
+        <BlurFade delay={BLUR_FADE_DELAY * 3} inView>
+          <Feature.Root>
+            <div className="aspect-square overflow-hidden">
+              {popularReviews.map((review) => (
+                <ReviewItem review={review} key={review.id} />
+              ))}
+            </div>
 
-          <Feature.Infos>
-            <Pencil className="size-6" />
-            <Feature.Title>Análises</Feature.Title>
-            <Feature.Description>Escreva análises tlgd</Feature.Description>
-          </Feature.Infos>
-        </Feature.Root>
+            <Feature.Infos>
+              <Pencil className="size-6" />
+              <Feature.Title>Análises</Feature.Title>
+              <Feature.Description>Escreva análises tlgd</Feature.Description>
+            </Feature.Infos>
+          </Feature.Root>
+        </BlurFade>
 
-        <Feature.Root>
-          <div className="aspect-square bg-muted"></div>
+        <BlurFade delay={BLUR_FADE_DELAY * 4} inView>
+          <Feature.Root>
+            <div className="aspect-square bg-muted">{}</div>
 
-          <Feature.Infos>
-            <List className="size-6" />
-            <Feature.Title>Listas</Feature.Title>
-            <Feature.Description>Elabore listas</Feature.Description>
-          </Feature.Infos>
-        </Feature.Root>
+            <Feature.Infos>
+              <List className="size-6" />
+              <Feature.Title>Listas</Feature.Title>
+              <Feature.Description>Elabore listas</Feature.Description>
+            </Feature.Infos>
+          </Feature.Root>
+        </BlurFade>
 
-        <Feature.Root>
-          <div className="aspect-square bg-muted"></div>
+        <BlurFade delay={BLUR_FADE_DELAY * 5} inView>
+          <Feature.Root>
+            <div className="aspect-square bg-muted"></div>
 
-          <Feature.Infos>
-            <Users className="size-6" />
-            <Feature.Title>Comunidades</Feature.Title>
-            <Feature.Description>Participe de comunidades</Feature.Description>
-          </Feature.Infos>
-        </Feature.Root>
+            <Feature.Infos>
+              <Users className="size-6" />
+              <Feature.Title>Comunidades</Feature.Title>
+              <Feature.Description>
+                Participe de comunidades
+              </Feature.Description>
+            </Feature.Infos>
+          </Feature.Root>
+        </BlurFade>
       </ul>
     </section>
   )
