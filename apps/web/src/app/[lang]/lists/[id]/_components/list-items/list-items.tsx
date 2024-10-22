515abc1b0220ab68d44a2a89623d469563d8bcc0
Vicentesan <vikom.sanchez@gmail.com>
2024-10-22 18:51:11 -0300

    fix(lists): only list author can edit list order

--- apps/web/src/app/[lang]/lists/[id]/_components/list-items/list-items.tsx
+++ apps/web/src/app/[lang]/lists/[id]/_components/list-items/list-items.tsx
@@ -10,15 +10,19 @@ import { ListItemsGrid } from './list-items-grid'
 import { DataTable, columns } from '../data-table'
 import { useListMode } from '@/context/list-mode'
 import { useState } from 'react'
+import { useAuth } from '@/context/auth'
 
 type ListItemsProps = {
+  ownerId: string
   listItems: ListItem[]
 }
-export const ListItems = ({ listItems }: ListItemsProps) => {
+export const ListItems = ({ ownerId, listItems }: ListItemsProps) => {
   const [layout, setLayout] = useState<'table' | 'grid'>('grid')
   const { dictionary, language } = useLanguage()
   const { mode } = useListMode()
   const [isEditable, setIsEditable] = useState(false)
+
+  const { user } = useAuth()
 
   const contentByLayout: Record<typeof layout, JSX.Element> = {
     grid: <ListItemsGrid listItems={listItems} isEditable={isEditable} />,
@@ -29,6 +33,8 @@ export const ListItems = ({ listItems }: ListItemsProps) => {
       />
     ),
   }
+
+  const isListAuthor = ownerId === user?.id
 
   return (
     <section className="space-y-4">
@@ -53,7 +59,11 @@ export const ListItems = ({ listItems }: ListItemsProps) => {
           </Button>
         </div>
         {layout === 'grid' && (
-          <Button variant={'ghost'} onClick={() => setIsEditable(!isEditable)}>
+          <Button
+            variant="outline"
+            onClick={() => setIsEditable(!isEditable)}
+            disabled={!isListAuthor}
+          >
             {isEditable ? dictionary.save_order : dictionary.edit_order}
           </Button>
         )}
