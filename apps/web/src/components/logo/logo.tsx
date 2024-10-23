4ef669df5e77ceff90159457ac06eba97aac14e4
Vicentesan <vikom.sanchez@gmail.com>
2024-10-23 13:40:54 -0300

    fix(auth): when user clicks in the header logo and its authenticated he is redirected to home page

--- apps/web/src/components/logo/logo.tsx
+++ apps/web/src/components/logo/logo.tsx
@@ -1,15 +1,21 @@
 'use client'
 
+import { authContext } from '@/context/auth'
 import { useLanguage } from '@/context/language'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useContext } from 'react'
 
 type LogoProps = { size?: number }
 export const Logo = ({ size = 24 }: LogoProps) => {
   const { language } = useLanguage()
 
+  const { user } = useContext(authContext)
+
+  const href = user ? `/${language}/home` : `/${language}/`
+
   return (
-    <Link href={`/${language}/`} className="text-foreground">
+    <Link href={href} className="text-foreground">
       <Image
         src="/logo-black.svg"
         alt="Logo"
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { match } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 import { languages as appLanguages } from '../languages'
+import { getUserService } from './services/api/users/get-user'
 
 const headers = { 'accept-language': 'en-US' }
 const languages = new Negotiator({ headers }).languages()
@@ -28,6 +29,16 @@ export async function middleware(req: NextRequest) {
   if (!pathnameHasLocale) {
     req.nextUrl.pathname = `/${language}${pathname}`
     return NextResponse.redirect(req.nextUrl)
+  }
+
+  const user = await getUserService()
+
+  const isPathnameOnlyLocale = appLanguages.some(
+    (locale) => pathname === `/${locale}`,
+  )
+
+  if (isPathnameOnlyLocale && user) {
+    return NextResponse.redirect(`${req.nextUrl}/home`)
   }
 
   return NextResponse.next()
