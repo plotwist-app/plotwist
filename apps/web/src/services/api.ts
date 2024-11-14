be389069c92a0e0389e3534d18adafc65d68cd99
Luiz Henrique <7henrique18@gmail.com>
2024-11-14 19:42:04 -0300

    fix: env

--- apps/web/src/services/api.ts
+++ apps/web/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-console.log({ pokedex: URL })
 
 export const api = axios.create({
-  baseURL: URL || 'http://localhost:3000/api',
+  baseURL: `http://${URL}/api` || 'http://localhost:3000/api',
 })
