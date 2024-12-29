218dc58d891719ea97151aa7538586f95b4e0484
Luiz Henrique <7henrique18@gmail.com>
2024-12-28 23:06:58 -0300

    fix: empty reviews

--- apps/web/src/components/reviews/reviews.tsx
+++ apps/web/src/components/reviews/reviews.tsx
@@ -9,6 +9,8 @@ import { useGetReviews } from '@/api/reviews'
 import { v4 } from 'uuid'
 
 import type { MediaType } from '@/types/media-type'
+import { ReviewFormDialog } from './review-form-dialog'
+import { useLanguage } from '@/context/language'
 
 export type ReviewsProps = {
   tmdbId: number
@@ -16,6 +18,7 @@ export type ReviewsProps = {
 }
 
 export const Reviews = ({ tmdbId, mediaType }: ReviewsProps) => {
+  const { dictionary } = useLanguage()
   const { data, isLoading } = useGetReviews({
     tmdbId: String(tmdbId),
     mediaType,
@@ -32,7 +35,20 @@ export const Reviews = ({ tmdbId, mediaType }: ReviewsProps) => {
     )
   }
 
-  if (!data) return <></>
+  if (!data || data.length === 0)
+    return (
+      <ReviewFormDialog tmdbId={tmdbId} mediaType={mediaType}>
+        <section className="text-center border border-dashed rounded-lg py-8 cursor-pointer">
+          <p className="text-sm lg:text-sm">
+            {dictionary.be_the_first_to_leave_your_opinion}
+          </p>
+
+          <p className="text-xs lg:text-sm text-muted-foreground">
+            {dictionary.click_here_and_share_your_experience}
+          </p>
+        </section>
+      </ReviewFormDialog>
+    )
 
   return (
     <section className="space-y-8">
