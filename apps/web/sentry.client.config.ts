3bc9551a5cfd453e1b86fe236f8eb4dbd36dacbd
Luiz Henrique <7henrique18@gmail.com>
2025-01-16 12:47:10 -0300

    refactor: remove Sentry integration and update package dependencies

--- apps/web/sentry.client.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs'
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  integrations: [Sentry.replayIntegration()],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  debug: false,
-})
