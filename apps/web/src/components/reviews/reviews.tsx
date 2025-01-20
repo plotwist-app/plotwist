f7468c6c77f105261ca55fda54988e42fbf76105
Luiz Henrique <7henrique18@gmail.com>
2025-01-20 14:51:12 -0300

    feat: add review form dialog for users to submit reviews when no reviews are available

--- apps/web/src/components/reviews/reviews.tsx
+++ apps/web/src/components/reviews/reviews.tsx
@@ -9,9 +9,8 @@ import { useGetReviews } from '@/api/reviews'
 import { v4 } from 'uuid'
 
 import type { MediaType } from '@/types/media-type'
+import { ReviewFormDialog } from './review-form-dialog'
 import { useLanguage } from '@/context/language'
-import { ReviewsSummary, ReviewsSummarySkeleton } from './reviews-summary'
-import { Suspense } from 'react'
 
 export type ReviewsProps = {
   tmdbId: number
@@ -19,7 +18,7 @@ export type ReviewsProps = {
 }
 
 export const Reviews = ({ tmdbId, mediaType }: ReviewsProps) => {
-  const { language } = useLanguage()
+  const { dictionary } = useLanguage()
   const { data, isLoading } = useGetReviews({
     tmdbId: String(tmdbId),
     mediaType,
@@ -36,14 +35,23 @@ export const Reviews = ({ tmdbId, mediaType }: ReviewsProps) => {
     )
   }
 
+  if (!data || data.length === 0)
+    return (
+      <ReviewFormDialog tmdbId={tmdbId} mediaType={mediaType}>
+        <section className="text-center border border-dashed rounded-lg py-8 cursor-pointer">
+          <p className="text-sm lg:text-sm">
+            {dictionary.be_the_first_to_leave_your_opinion}
+          </p>
+
+          <p className="text-xs lg:text-sm text-muted-foreground">
+            {dictionary.click_here_and_share_your_experience}
+          </p>
+        </section>
+      </ReviewFormDialog>
+    )
+
   return (
     <section className="space-y-6">
-      {mediaType === 'MOVIE' && (
-        <Suspense fallback={<ReviewsSummarySkeleton />}>
-          <ReviewsSummary tmdbId={tmdbId} language={language} />
-        </Suspense>
-      )}
-
       {data
         ?.filter(review => review.review)
         .map(review => (
