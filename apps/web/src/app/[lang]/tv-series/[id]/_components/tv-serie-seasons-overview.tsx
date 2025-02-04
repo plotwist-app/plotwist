c34438465c4015413e164f40c04150dc8369ae5c
Luiz Henrique <7henrique18@gmail.com>
2025-02-04 20:09:34 -0300

    Task/tv series overview (#410)
    * feat: add seasons overview view with rating-based color-coded badges
    
    * refactor: improve TV series seasons view with ScrollArea and styling updates
    
    * feat: add overview localization and update TV series seasons view

---
+++ apps/web/src/app/[lang]/tv-series/[id]/_components/tv-serie-seasons-overview.tsx
@@ -0,0 +1,213 @@
+'use client'
+
+import type { Language, Season } from '@plotwist_app/tmdb'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@plotwist/ui/components/ui/table'
+import { v4 } from 'uuid'
+import { tmdb } from '@/services/tmdb'
+import { useLanguage } from '@/context/language'
+import { useParams } from 'next/navigation'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Badge } from '@plotwist/ui/components/ui/badge'
+import { cn } from '@/lib/utils'
+import { ScrollArea, ScrollBar } from '@plotwist/ui/components/ui/scroll-area'
+import { Skeleton } from '@plotwist/ui/components/ui/skeleton'
+
+type TvSerieSeasonsOverviewProps = {
+  seasons: Season[]
+}
+
+async function getSeasonDetails(
+  tvSerieId: number,
+  seasons: Season[],
+  language: Language
+) {
+  const seasonsDetails = await Promise.all(
+    seasons.map(
+      async season =>
+        await tmdb.season.details(tvSerieId, season.season_number, language)
+    )
+  )
+
+  const formattedSeasonsDetails = seasonsDetails.map(season => {
+    return {
+      ...season,
+      episodes: season.episodes.map((episode, episodeIndex) => ({
+        ...episode,
+        episode_number: episodeIndex + 1,
+      })),
+    }
+  })
+
+  return formattedSeasonsDetails
+}
+
+export function TvSerieSeasonsOverview({
+  seasons,
+}: TvSerieSeasonsOverviewProps) {
+  const params = useParams<{ id: string }>()
+  const { language } = useLanguage()
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['tv-serie-seasons-details', params.id, language],
+    queryFn: () => getSeasonDetails(Number(params.id), seasons, language),
+  })
+
+  const episodesLengths = data.map(season => season.episodes.length)
+  const maxEpisodes = Math.max(...episodesLengths)
+
+  return (
+    <ScrollArea className="!overflow-auto relative h-[80vh] max-w-4xl ">
+      <Table>
+        <TableHeader className="sticky top-0 bg-background z-10 after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-border">
+          <TableRow className="border-0 text-xs">
+            <TableHead
+              className={cn(
+                'p-2 text-center sticky top-0 left-0 bg-background',
+                'after:absolute after:right-0 after:w-[1px] after:h-[120%] after:top-0 after:bg-border'
+              )}
+            >
+              EP
+            </TableHead>
+
+            {seasons.map(season => (
+              <TableHead key={season.season_number} className="p-2 text-center">
+                S{season.season_number}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+
+        <TableBody className="relative">
+          {[...Array(maxEpisodes)].map((_, episodeIndex) => (
+            <TableRow key={v4()}>
+              <TableCell
+                className={cn(
+                  'p-2 text-center font-medium sticky left-0 bg-background text-muted-foreground text-xs',
+                  'after:absolute after:right-0 after:w-[1px] after:h-[120%] after:top-0 after:bg-border'
+                )}
+              >
+                {episodeIndex + 1}
+              </TableCell>
+
+              {seasons.map(season => {
+                const seasonDetails = data.find(
+                  seasonDetail =>
+                    seasonDetail.season_number === season.season_number
+                )
+
+                const episode = seasonDetails?.episodes.find(
+                  episode => episode.episode_number === episodeIndex + 1
+                )
+
+                if (!episode || episode.vote_average === 0) {
+                  return (
+                    <TableCell
+                      key={season.season_number}
+                      className="p-2 text-center text-sm text-muted-foreground"
+                    >
+                      -
+                    </TableCell>
+                  )
+                }
+
+                return (
+                  <TableCell
+                    key={season.season_number}
+                    className="p-2 text-center text-sm"
+                  >
+                    <Badge
+                      className={cn(
+                        'font-medium',
+                        episode.vote_average > 0 &&
+                          'bg-red-200 text-red-900 hover:bg-red-300 dark:bg-red-700/50 dark:text-red-100 dark:hover:bg-red-700',
+                        episode.vote_average > 2 &&
+                          'bg-orange-200 text-orange-900 hover:bg-orange-300 dark:bg-orange-700/50 dark:text-orange-100 dark:hover:bg-orange-700',
+                        episode.vote_average > 4 &&
+                          'bg-yellow-200 text-yellow-900 hover:bg-yellow-300 dark:bg-yellow-700/50 dark:text-yellow-100 dark:hover:bg-yellow-700',
+                        episode.vote_average > 6 &&
+                          'bg-lime-200 text-lime-900 hover:bg-lime-300 dark:bg-lime-700/50 dark:text-lime-100 dark:hover:bg-lime-700',
+                        episode.vote_average > 8 &&
+                          'bg-green-200 text-green-900 hover:bg-green-300 dark:bg-green-700/50 dark:text-green-100 dark:hover:bg-green-700'
+                      )}
+                    >
+                      {episode?.vote_average.toFixed(1)}
+                    </Badge>
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  )
+}
+
+type TvSerieSeasonsOverviewSkeletonProps = {
+  seasons: Season[]
+}
+
+export function TvSerieSeasonsOverviewSkeleton({
+  seasons,
+}: TvSerieSeasonsOverviewSkeletonProps) {
+  const maxEpisodes = Math.max(...seasons.map(season => season.episode_count))
+
+  return (
+    <Table>
+      <TableHeader className="sticky top-0 bg-background z-10 after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-border">
+        <TableRow className="border-0 text-xs">
+          <TableHead
+            className={cn(
+              'p-2 text-center sticky top-0 left-0 bg-background',
+              'after:absolute after:right-0 after:w-[1px] after:h-[120%] after:top-0 after:bg-border'
+            )}
+          >
+            EP
+          </TableHead>
+
+          {seasons.map(season => (
+            <TableHead key={season.season_number} className="p-2 text-center">
+              S{season.season_number}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+
+      <TableBody className="relative">
+        {Array.from({ length: maxEpisodes }).map((_, episodeIndex) => (
+          <TableRow key={v4()}>
+            <TableCell
+              className={cn(
+                'p-2 text-center font-medium sticky left-0 bg-background text-muted-foreground text-xs',
+                'after:absolute after:right-0 after:w-[1px] after:h-[120%] after:top-0 after:bg-border'
+              )}
+            >
+              {episodeIndex + 1}
+            </TableCell>
+
+            {seasons.map(season => {
+              return (
+                <TableCell
+                  key={season.season_number}
+                  className="p-2 text-center text-sm"
+                >
+                  <Skeleton className="w-[40px] h-[22px] m-auto" />
+                </TableCell>
+              )
+            })}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
--- apps/web/src/app/[lang]/tv-series/[id]/_components/tv-serie-seasons.tsx
+++ apps/web/src/app/[lang]/tv-series/[id]/_components/tv-serie-seasons.tsx
@@ -1,9 +1,18 @@
-import type { Season } from '@/services/tmdb'
+'use client'
 
-import { Poster } from '@/components/poster'
+import type { Season } from '@/services/tmdb'
 import type { Language } from '@/types/languages'
+import { Poster } from '@/components/poster'
 
 import { Link } from 'next-view-transitions'
+import { Suspense, useState } from 'react'
+import { Button } from '@plotwist/ui/components/ui/button'
+import { Eye, Grid } from 'lucide-react'
+import {
+  TvSerieSeasonsOverview,
+  TvSerieSeasonsOverviewSkeleton,
+} from './tv-serie-seasons-overview'
+import { useLanguage } from '@/context/language'
 
 type TvSerieSeasonsProps = {
   seasons: Season[]
@@ -16,25 +25,67 @@ export const TvSerieSeasons = ({
   language,
   id,
 }: TvSerieSeasonsProps) => {
+  const { dictionary } = useLanguage()
+  const [variant, setVariant] = useState<'grid' | 'overview'>('grid')
+
   const filteredSeasons = seasons.filter(
-    season =>
-      season.season_number !== 0 &&
-      season.episode_count > 0 &&
-      season.air_date !== null
+    season => season.season_number !== 0 && season.episode_count > 0
   )
 
+  const renderContent = () => {
+    if (variant === 'grid') {
+      return (
+        <div className="grid grid-cols-3 gap-4 md:grid-cols-5 items-start">
+          {filteredSeasons.map(
+            ({ poster_path: poster, name, season_number: seasonNumber }) => (
+              <Link
+                href={`/${language}/tv-series/${id}/seasons/${seasonNumber}`}
+                key={seasonNumber}
+              >
+                <Poster url={poster} alt={name} />
+              </Link>
+            )
+          )}
+        </div>
+      )
+    }
+
+    return (
+      <>
+        <Suspense
+          fallback={
+            <TvSerieSeasonsOverviewSkeleton seasons={filteredSeasons} />
+          }
+        >
+          <TvSerieSeasonsOverview seasons={filteredSeasons} />
+        </Suspense>
+      </>
+    )
+  }
+
   return (
-    <div className="grid grid-cols-3 gap-4 md:grid-cols-5 items-start">
-      {filteredSeasons.map(
-        ({ poster_path: poster, name, season_number: seasonNumber }) => (
-          <Link
-            href={`/${language}/tv-series/${id}/seasons/${seasonNumber}`}
-            key={seasonNumber}
-          >
-            <Poster url={poster} alt={name} />
-          </Link>
-        )
-      )}
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-2">
+        <Button
+          variant={variant === 'grid' ? 'default' : 'outline'}
+          size="sm"
+          onClick={() => setVariant('grid')}
+        >
+          <Grid className="size-4 mr-2" />
+          {dictionary.grid}
+        </Button>
+
+        <Button
+          variant={variant === 'overview' ? 'default' : 'outline'}
+          size="sm"
+          onClick={() => setVariant('overview')}
+        >
+          <Eye className="size-4 mr-2" />
+          {dictionary.overview}
+        </Button>
+      </div>
+
+      {renderContent()}
     </div>
   )
 }
