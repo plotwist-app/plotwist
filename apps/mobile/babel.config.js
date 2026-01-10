2efaaa770775466c7b7cc1dcca6df8578315a299
henrique <7henrique18@gmail.com>
2026-01-09 21:41:22 -0300

    feat(mobile): initialize React Native app structure and configuration

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,9 +1,13 @@
-module.exports = function (api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+    ],
+    plugins: [
+      // NOTE: This must be listed last
+      "react-native-reanimated/plugin",
     ],
   };
 };
