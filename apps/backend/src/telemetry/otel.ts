4da1951099b8300f947a99dd5393457ffff4da18
João Pedro Alves <joaopealves.dev@gmail.com>
2026-02-17 21:13:03 -0300

    chore: add grafana image

---
+++ apps/backend/src/telemetry/otel.ts
@@ -0,0 +1,28 @@
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
+import { resourceFromAttributes } from '@opentelemetry/resources'
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions'
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: 'plotwist-api',
+    [ATTR_SERVICE_VERSION]: '0.1.0',
+  }),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+    headers: {},
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4318/v1/metrics',
+      headers: {},
+    }),
+  }),
+})
+
+sdk.start()
