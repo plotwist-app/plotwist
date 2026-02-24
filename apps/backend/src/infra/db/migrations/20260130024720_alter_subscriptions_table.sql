2a3d01a8952109030662e3fddd0a8a96f812dc0b
João Pedro Alves <joaopealves.dev@gmail.com>
2026-02-23 23:25:40 -0300

    chore: remove obsolete migration files for subscriptions table

--- apps/backend/src/infra/db/migrations/20260130024720_alter_subscriptions_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE "public"."subscription_provider" AS ENUM('STRIPE', 'APPLE');--> statement-breakpoint
-ALTER TABLE "subscriptions" ADD COLUMN "subscription_provider" "subscription_provider" NOT NULL DEFAULT 'STRIPE';--> statement-breakpoint
-ALTER TABLE "subscriptions" ADD COLUMN "provider_subscription_id" varchar;--> statement-breakpoint
-ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_provider_subscription_id_unique" UNIQUE("provider_subscription_id");
