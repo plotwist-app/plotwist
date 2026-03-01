75dcc4304ba506cf27aec9e3674717c48b4d2bda
henrique <7henrique18@gmail.com>
2026-03-01 16:14:54 -0300

    feat(next.config): enable unoptimized image loading for improved performance

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
         hostname: 'image.tmdb.org',
       },
     ],
+    unoptimized: true,
   },
   pageExtensions: ['mdx', 'ts', 'tsx'],
   transpilePackages: ['@plotwist/ui'],
